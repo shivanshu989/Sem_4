252ae488305596ac7b147823526f7b56954bf4b8
shivanshu989 <81761392+shivanshu989@users.noreply.github.com>
2022-01-08 13:06:39 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Employee {
+    String name;
+    int id;
+    int sal;
+    Employee(){
+        Scanner cin=new Scanner(System.in);
+        name=cin.nextLine();
+        id=cin.nextInt();
+        sal=cin.nextInt();
+    }
+}
+
+class Next{
+    public static void main(String[] args) {
+        Employee a[]=new Employee[10];
+        for(int i=0;i<a.length;i++){
+            a[i]=new Employee();
+        }
+        int x=a[0].sal;
+        int y=a[0].sal;
+        int f=0;
+        for(int i=1;i<a.length;i++){
+            if(a[i].sal>x){
+                x=a[i].sal;
+            }
+            else if(a[i].sal>y){
+                y=a[i].sal;
+                f=i;
+            }
+            else{
+                continue;
+            }
+        }
+        System.out.println(a[f].name+" "+a[f].id+" "+a[f].sal);
+    }
+}
